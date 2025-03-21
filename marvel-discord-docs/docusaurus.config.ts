bd3d86615ae4a6a497a59a296b99ecf6bb61e330
Rocked03 <rockedohthree@gmail.com>
2025-03-21 22:23:08 +1300

    feat: applied branding and setup layout

--- marvel-discord-docs/docusaurus.config.ts
+++ marvel-discord-docs/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-	title: "My Site",
-	tagline: "Dinosaurs are cool",
+	title: "Marvel Discord Staff Docs",
+	tagline: "Watchers of the House of Ideas",
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
@@ -17,8 +17,8 @@ const config: Config = {
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
+	organizationName: "Rocked03", // Usually your GitHub org/user name.
+	projectName: "marvel-discord-docs", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
@@ -39,8 +39,8 @@ const config: Config = {
 					sidebarPath: "./sidebars.ts",
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					editUrl: "https://github.com/Rocked03/marvel-discord-docs",
+					routeBasePath: "/",
 				},
 				theme: {
 					customCss: "./src/css/custom.css",
@@ -51,23 +51,29 @@ const config: Config = {
 
 	themeConfig: {
 		// Replace with your project's social card
-		image: "img/docusaurus-social-card.jpg",
+		image: "img/icon-red-filled-square.svg",
 		navbar: {
-			title: "My Site",
+			title: "Marvel Discord",
 			logo: {
 				alt: "My Site Logo",
-				src: "img/logo.svg",
+				src: "img/icon-red-filled-square.svg",
 			},
 			items: [
 				{
 					type: "docSidebar",
-					sidebarId: "tutorialSidebar",
+					sidebarId: "moderation",
 					position: "left",
-					label: "Tutorial",
+					label: "Moderation",
 				},
 				{
-					href: "https://github.com/facebook/docusaurus",
-					label: "GitHub",
+					type: "docSidebar",
+					sidebarId: "management",
+					position: "left",
+					label: "Management",
+				},
+				{
+					href: "https://github.com/Rocked03/marvel-discord-docs",
+					label: "Docs Source",
 					position: "right",
 				},
 			],
@@ -79,8 +85,12 @@ const config: Config = {
 					title: "Docs",
 					items: [
 						{
-							label: "Tutorial",
-							to: "/docs/intro",
+							label: "Moderation docs",
+							to: "/moderation",
+						},
+						{
+							label: "Management docs",
+							to: "/management",
 						},
 					],
 				},
@@ -88,16 +98,16 @@ const config: Config = {
 					title: "Community",
 					items: [
 						{
-							label: "Stack Overflow",
-							href: "https://stackoverflow.com/questions/tagged/docusaurus",
+							label: "Marvel Discord invite",
+							href: "https://discord.gg/marvel",
 						},
 						{
-							label: "Discord",
-							href: "https://discordapp.com/invite/docusaurus",
+							label: "Post Office invite",
+							href: "https://discord.gg/ppm8DXZxKp",
 						},
 						{
-							label: "X",
-							href: "https://x.com/docusaurus",
+							label: "Moderation server",
+							href: "https://discord.com/channels/915931105977319455/915931106426097716",
 						},
 					],
 				},
@@ -105,13 +115,17 @@ const config: Config = {
 					title: "More",
 					items: [
 						{
+							label: "Discord Web Page",
+							href: "https://discord.com/servers/marvel-discord-281648235557421056",
+						},
+						{
 							label: "GitHub",
-							href: "https://github.com/facebook/docusaurus",
+							href: "https://github.com/Rocked03/marvel-discord-docs",
 						},
 					],
 				},
 			],
-			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+			copyright: `Copyright © ${new Date().getFullYear()} Marvel Discord. Built with Docusaurus.`,
 		},
 		prism: {
 			theme: prismThemes.github,
--- marvel-discord-docs/src/components/HomepageFeatures/index.tsx
+++ marvel-discord-docs/src/components/HomepageFeatures/index.tsx
@@ -1,54 +1,49 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Everything useful to know",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The goal of these docs is to provide a comprehensive guide to everything
+        in the Marvel Discord server.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Made for staff",
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        This documentation is geared towards information for managers and
+        moderators of the server. The whole site is publicly-accessible, but it
+        isn't made to be a public resource.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Please contribute!",
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        If there's information missing, incorrect, outdated, or could be more
+        detailed: please contribute! Instructions on how to contribute can be
+        found in the <a href="/docs/contributing">Contributing</a> section.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
+    <div className={clsx("col col--4")}>
+      <div className="text--center padding-horiz--md padding-vert--lg">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
@@ -61,8 +56,8 @@ export default function HomepageFeatures(): ReactNode {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+          {FeatureList.map((props) => (
+            <Feature key={props.title} {...props} />
           ))}
         </div>
       </div>
