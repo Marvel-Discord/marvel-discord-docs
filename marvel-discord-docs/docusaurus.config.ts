874d102c00c46e1777e97f886a26182ec691e76e
Rocked03 <rockedohthree@gmail.com>
2025-03-21 23:49:53 +1300

    chore: netlify support

--- marvel-discord-docs/docusaurus.config.ts
+++ marvel-discord-docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://your-docusaurus-site.example.com",
+	url: "https://marvel-discord-docs.netlify.app/",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "marvel-discord-docs/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
